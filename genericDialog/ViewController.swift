41bc9e4731d177a274af27ec1497f4d93266ed2a
Helio Mesquita <mesquitahelio@hotmail.com>
2017-11-30 07:40:32 -0200

    adding backgorund

--- genericDialog/ViewController.swift
+++ genericDialog/ViewController.swift
@@ -1,25 +1,48 @@
-//
-//  ViewController.swift
-//  genericDialog
-//
-//  Created by Hélio Mesquita on 30/11/2017.
-//  Copyright © 2017 Hélio Mesquita. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  @IBAction func buttonAction(_ sender: Any) {
+    self.view.addSubview(Dialog(frame: .zero))
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
+class Dialog: UIView {
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    translatesAutoresizingMaskIntoConstraints = false
+    configureBackground()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func updateConstraints() {
+    super.updateConstraints()
+    var constraints = [NSLayoutConstraint]()
+    constraints.append(self.topAnchor.constraint(equalTo: superview!.topAnchor))
+    constraints.append(self.trailingAnchor.constraint(equalTo: superview!.trailingAnchor))
+    constraints.append(self.leadingAnchor.constraint(equalTo: superview!.leadingAnchor))
+    constraints.append(self.bottomAnchor.constraint(equalTo: superview!.bottomAnchor))
+    NSLayoutConstraint.activate(constraints)
+  }
+
+  func configureBackground() {
+    backgroundColor = UIColor.black
+    alpha = 0.5
+    let dismissGesture = UITapGestureRecognizer(target: self, action:  #selector(dismissAction))
+    self.addGestureRecognizer(dismissGesture)
+  }
+
+  func dismissAction(sender: UITapGestureRecognizer) {
+    UIView.animate(withDuration: 0.3, animations: {
+      self.alpha = 0
+    }) { (hasFinished) in
+      if hasFinished {
+        self.removeFromSuperview()
+      }
+    }
+  }
+
+}
