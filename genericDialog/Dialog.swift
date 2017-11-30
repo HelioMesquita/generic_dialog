b5cdd227a6be78f88dbf96bb6572b8e52d3fdff4
Lucas Pedrazoli <lucaspedrazoli@gmail.com>
2017-11-30 18:02:33 -0200

    adding outlets

--- genericDialog/Dialog.swift
+++ genericDialog/Dialog.swift
@@ -2,7 +2,12 @@ import UIKit
 
 class Dialog: UIView {
 
-   var filledButton: UIButton!
+  var filledButton: UIButton!
+  var containerView: UIView!
+  var titleLabel: UILabel!
+  var descriptionLabel: UILabel!
+  var imageView: UIImageView!
+  var backgroundView: UIView!
 
   init() {
     super.init(frame: .zero)
@@ -37,7 +42,7 @@ class Dialog: UIView {
   }
 
   private func insertBackgroundView() {
-    let backgroundView = UIView(frame: .zero)
+    backgroundView = UIView(frame: .zero)
     backgroundView.backgroundColor = UIColor.black
     let dismissGesture = UITapGestureRecognizer(target: self, action:  #selector(dismissAction))
     backgroundView.addGestureRecognizer(dismissGesture)
@@ -52,7 +57,7 @@ class Dialog: UIView {
   }
 
   private func insertContainer() {
-    let containerView = UIView(frame: .zero)
+    containerView = UIView(frame: .zero)
     containerView.backgroundColor = UIColor.white
     containerView.accessibilityIdentifier = "containerView"
     containerView.layer.cornerRadius = 6
@@ -68,11 +73,9 @@ class Dialog: UIView {
   }
 
   func insertImageView(_ image: UIImage) {
-    let imageView = UIImageView(frame: .zero)
+    imageView = UIImageView(frame: .zero)
     imageView.contentMode = .scaleAspectFit
-    imageView.accessibilityIdentifier = "imageView"
     imageView.image = image
-    let containerView = getContainerView()
     containerView.addSubview(imageView)
     imageView.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
@@ -83,37 +86,31 @@ class Dialog: UIView {
   }
 
   func insertTitle(_ title: String) {
-    let label = UILabel(frame: .zero)
-    label.text = title
-    label.textColor = UIColor.black
-    label.textAlignment = .center
-    label.accessibilityIdentifier = "titleLabel"
-    label.numberOfLines = 2
-    let containerView = getContainerView()
-    containerView.addSubview(label)
-    label.translatesAutoresizingMaskIntoConstraints = false
+    titleLabel = UILabel(frame: .zero)
+    titleLabel.text = title
+    titleLabel.textColor = UIColor.black
+    titleLabel.textAlignment = .center
+    titleLabel.numberOfLines = 2
+    containerView.addSubview(titleLabel)
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
-    let image = getImage()
-    constraints.append(label.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 24))
-    constraints.append(label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor))
-    constraints.append(label.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16))
-    constraints.append(label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16))
+    constraints.append(titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 24))
+    constraints.append(titleLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor))
+    constraints.append(titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16))
+    constraints.append(titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16))
     NSLayoutConstraint.activate(constraints)
   }
 
   func insertDescription(_ description: String) {
-    let descriptionLabel = UILabel(frame: .zero)
+    descriptionLabel = UILabel(frame: .zero)
     descriptionLabel.text = description
     descriptionLabel.textColor = UIColor.black
     descriptionLabel.numberOfLines = 0
     descriptionLabel.textAlignment = .center
-    descriptionLabel.accessibilityIdentifier = "descriptionLabel"
-    let containerView = getContainerView()
     containerView.addSubview(descriptionLabel)
     descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
-    let title = getTitle()
-    constraints.append(descriptionLabel.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 16))
+    constraints.append(descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16))
     constraints.append(descriptionLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor))
     constraints.append(descriptionLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16))
     constraints.append(descriptionLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16))
@@ -128,11 +125,9 @@ class Dialog: UIView {
     filledButton.accessibilityIdentifier = "firstButton"
     filledButton.layer.cornerRadius = 4
     filledButton.translatesAutoresizingMaskIntoConstraints = false
-    let containerView = getContainerView()
     containerView.addSubview(filledButton)
     var constraints = [NSLayoutConstraint]()
-    let description = getDescription()
-    let buttonTop = filledButton.topAnchor.constraint(equalTo: description.bottomAnchor, constant: 24)
+    let buttonTop = filledButton.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 24)
     buttonTop.identifier = "buttonTop"
     constraints.append(buttonTop)
     let containerBottom = containerView.bottomAnchor.constraint(equalTo: filledButton.bottomAnchor, constant: 20)
@@ -150,16 +145,13 @@ class Dialog: UIView {
     borderedButton.accessibilityIdentifier = "secondButton"
     borderedButton.layer.borderWidth = 1
     borderedButton.layer.cornerRadius = 4
-    let dialogBox = getContainerView()
-    dialogBox.addSubview(borderedButton)
+    containerView.addSubview(borderedButton)
     var constraints = [NSLayoutConstraint]()
-    let description = getDescription()
-    let buttonTop = dialogBox.constraints.first(where: { constraint in
+    let buttonTop = containerView.constraints.first(where: { constraint in
       return constraint.identifier == "buttonTop"
     })
     NSLayoutConstraint.deactivate([buttonTop!])
-    let containerView = getContainerView()
-    constraints.append(borderedButton.topAnchor.constraint(equalTo: description.bottomAnchor, constant: 24))
+    constraints.append(borderedButton.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 24))
     constraints.append(borderedButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16))
     constraints.append(borderedButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16))
     constraints.append(filledButton.topAnchor.constraint(equalTo: borderedButton.bottomAnchor, constant: 24))
@@ -174,41 +166,6 @@ class Dialog: UIView {
         self.removeFromSuperview()
       }
     }
-  }
-
-  private func getContainerView() -> UIView {
-    let containerView = self.subviews.first { (view) -> Bool in
-      return view.accessibilityIdentifier == "containerView"
-    }
-    return containerView!
-  }
-
-  private func getImage() -> UIView {
-    let image = getContainerView().subviews.first { (view) -> Bool in
-      return view.accessibilityIdentifier == "imageView"
-    }
-    return image!
-  }
-
-  private func getTitle() -> UIView {
-    let title = getContainerView().subviews.first { (view) -> Bool in
-      return view.accessibilityIdentifier == "titleLabel"
-    }
-    return title!
-  }
-
-  private func getDescription() -> UIView {
-    let description = getContainerView().subviews.first { (view) -> Bool in
-      return view.accessibilityIdentifier == "descriptionLabel"
-    }
-    return description!
-  }
-
-  func getBorderedButton() -> UIButton {
-    let button = getContainerView().subviews.first { (view) -> Bool in
-      return view.accessibilityIdentifier == "secondButton"
-    }
-    return button! as! UIButton
   }
 }
 
