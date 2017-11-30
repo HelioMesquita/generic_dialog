515c606b4c56f5abd741ce8d725d6f208dd1e29f
Helio Mesquita <mesquitahelio@hotmail.com>
2017-11-30 07:16:19 -0200

    Initial Commit

---
+++ genericDialog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  genericDialog
+//
+//  Created by Hélio Mesquita on 30/11/2017.
+//  Copyright © 2017 Hélio Mesquita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
