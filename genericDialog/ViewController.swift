3aa5ed2dc951ebbfc76c93c90b77d436964d459d
Helio Mesquita <mesquitahelio@hotmail.com>
2017-11-30 13:35:23 -0200

    improving methods

--- genericDialog/ViewController.swift
+++ genericDialog/ViewController.swift
@@ -46,36 +46,37 @@ class Dialog: UIView {
     self.addSubview(backgroundView)
     backgroundView.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
-    constraints.append(backgroundView.topAnchor.constraint(equalTo: superview!.topAnchor))
-    constraints.append(backgroundView.leadingAnchor.constraint(equalTo: superview!.leadingAnchor))
-    constraints.append(backgroundView.trailingAnchor.constraint(equalTo: superview!.trailingAnchor))
-    constraints.append(backgroundView.bottomAnchor.constraint(equalTo: superview!.bottomAnchor))
+    constraints.append(backgroundView.topAnchor.constraint(equalTo: self.topAnchor))
+    constraints.append(backgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor))
+    constraints.append(backgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor))
+    constraints.append(backgroundView.bottomAnchor.constraint(equalTo: self.bottomAnchor))
     NSLayoutConstraint.activate(constraints)
   }
 
   private func insertContainer() {
     let containerView = UIView(frame: .zero)
     containerView.backgroundColor = UIColor.white
-    containerView.translatesAutoresizingMaskIntoConstraints = false
     containerView.accessibilityIdentifier = "containerView"
     containerView.layer.cornerRadius = 6
     self.addSubview(containerView)
+    containerView.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
     constraints.append(containerView.centerXAnchor.constraint(equalTo: self.centerXAnchor))
     constraints.append(containerView.centerYAnchor.constraint(equalTo: self.centerYAnchor))
     constraints.append(containerView.heightAnchor.constraint(greaterThanOrEqualToConstant: 150))
-    constraints.append(containerView.widthAnchor.constraint(equalToConstant: superview!.frame.width - 32)) // ??
+    constraints.append(containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16))
+    constraints.append(containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16))
     NSLayoutConstraint.activate(constraints)
   }
 
   func insertImageView() {
     let imageView = UIImageView(frame: .zero)
-    imageView.translatesAutoresizingMaskIntoConstraints = false
     imageView.contentMode = .scaleAspectFit
     imageView.accessibilityIdentifier = "imageView"
     imageView.image = #imageLiteral(resourceName: "iconHeader")
     let containerView = getContainerView()
     containerView.addSubview(imageView)
+    imageView.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
     constraints.append(imageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20))
     constraints.append(imageView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor))
@@ -85,15 +86,15 @@ class Dialog: UIView {
   
   func insertTitle() {
     let label = UILabel(frame: .zero)
-    label.translatesAutoresizingMaskIntoConstraints = false
     label.text = "TESTANDO"
     label.textColor = UIColor.black
     label.textAlignment = .center
     label.accessibilityIdentifier = "titleLabel"
+    label.numberOfLines = 2
     let containerView = getContainerView()
     containerView.addSubview(label)
+    label.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
-
     if let image = getImage() {
       constraints.append(label.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 20))
     } else {
@@ -101,48 +102,47 @@ class Dialog: UIView {
     }
     constraints.append(label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor))
     constraints.append(label.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16))
+    constraints.append(label.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16))
     NSLayoutConstraint.activate(constraints)
   }
 
   func insertDescription() {
     let description = UILabel(frame: .zero)
-    description.translatesAutoresizingMaskIntoConstraints = false
-    description.text = "TESTANDO DESCRIPTION"
+    description.text = "TESTANDO DESCRIPTIONTEST"
     description.textColor = UIColor.black
+    description.numberOfLines = 0
     description.textAlignment = .center
     description.accessibilityIdentifier = "descriptionLabel"
-
     let containerView = getContainerView()
     containerView.addSubview(description)
+    description.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
-
     if let title = getTitle() {
       constraints.append(description.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 10))
     } else {
       constraints.append(description.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20))
     }
     constraints.append(description.centerXAnchor.constraint(equalTo: containerView.centerXAnchor))
+    constraints.append(description.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16))
     constraints.append(description.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16))
     NSLayoutConstraint.activate(constraints)
   }
 
   func insertButton() {
     let button = UIButton(frame: .zero)
-    button.translatesAutoresizingMaskIntoConstraints = false
     button.setTitle("Butao", for: .normal)
     button.setTitleColor(UIColor.black, for: .normal)
     button.accessibilityIdentifier = "button"
-
     let containerView = getContainerView()
     containerView.addSubview(button)
+    button.translatesAutoresizingMaskIntoConstraints = false
     var constraints = [NSLayoutConstraint]()
-
     if let description = getDescription() {
       constraints.append(button.topAnchor.constraint(equalTo: description.bottomAnchor, constant: 10))
     } else {
       constraints.append(button.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20))
     }
-    constraints.append(button.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 20))
+    constraints.append(containerView.bottomAnchor.constraint(equalTo: button.bottomAnchor, constant: 20))
     constraints.append(button.centerXAnchor.constraint(equalTo: containerView.centerXAnchor))
     constraints.append(button.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16))
     NSLayoutConstraint.activate(constraints)
